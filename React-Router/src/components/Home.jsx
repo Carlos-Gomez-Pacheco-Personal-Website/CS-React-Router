4bd211e9c2129bc3c45917475ed7146602a85214
Carlos Gomez <carlosegomez001@hotmail.com>
2024-02-08 23:02:23 -0500

    update

---
+++ React-Router/src/components/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="home-title">
+        <h1>Welcome to Our Website!</h1>
+      </div>
+      <div className="home-description">
+        <p>
+          We are glad to have you here. This is a demo website built with React
+          and React Router. Navigate through the links in the navbar to explore
+          more.
+        </p>
+      </div>
+    </div>
+  );
+}
